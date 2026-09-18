ad8f043ed8440cc53cd7219ec095826ef86196fa
clusko29-hash <clusko29@setoncatholic.org>
2026-09-18 16:05:08 +0000

    Extra Credit.

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ age=input("How old are you? ")
 print(age)
 color=input("What is your favorite color? ")
 print(color)
-print("Hello ",name, "you are ",age, "your favorite color is",color, ".")
+
+age_num = int(age)
+if age_num > 40:
+    print("Hello ", name, "you are ", age, "your favorite color is", color, ". Are you sure u can use a computer?")
+else:
+    print("Hello ", name, "you are ", age, "your favorite color is", color, ".")
